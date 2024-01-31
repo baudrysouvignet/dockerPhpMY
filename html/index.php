f6a380416aceb032c5cf3e79ac8fa14e70c6dd45
Baudry Souvignet <baudrysouvignet07@gmail.com>
2024-01-31 19:23:15 +0100

    Ajout de PhpMyAdmin

--- html/index.php
+++ html/index.php
@@ -2,7 +2,24 @@
 $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $dbname = getenv('DB_NAME');
-$password = getenv('DB_PASSWORD');
-$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+$password = getenv('DB_PASSWORD'); 
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
 
+$stmt = $pdo->query("SELECT * FROM utilisateur");
+// Récupérer les résultats dans un tableau associatif
+$utilisateurs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Afficher les informations des utilisateurs avec une boucle foreach
+foreach ($utilisateurs as $utilisateur) {
+    echo "ID: " . $utilisateur['id'] . "<br>";
+    echo "Nom: " . $utilisateur['nom'] . "<br>";
+    echo "Email: " . $utilisateur['email'] . "<br>";
+    // Ajoutez d'autres champs selon votre structure de base de données
+    echo "<hr>";
+}
